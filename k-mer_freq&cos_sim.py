6f6fefa83d7560c8a97045a1392fbbdf8b5515e8
Samar Kodwani <samarkodwani9@gmail.com>
2026-05-09 23:21:46 +0530

    k-mer_freq&cos_sim.py

--- k-mer_freq&cos_sim.py
+++ k-mer_freq&cos_sim.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
+#function that returns a dictionary having sequence name - sequence pairs
+#download a multi-FASTA file from the NCBI database (optional: save it as seqdump.txt)
+
 def cleaning(text):
     my_dict = {}
     with open(text, 'r') as file:
@@ -16,6 +19,8 @@ def cleaning(text):
                 my_dict[temp] += line.strip()
     return my_dict
 
+#function for counting a frequencies dictionary
+
 def kf(seq, k):
     my_dic = {}
     for i in range(len(seq) - k+1):
@@ -30,9 +35,10 @@ def kf(seq, k):
 # for i, j in dic.items():
 #     print(f'the freq for the sequence ({i}) is:\n')
 #     print(f'{kf(j,3)}\n')
+#outputs the k-mer frequencies for every sequence in the file 'seqdump.txt'
 
 
-
+#function that calculates the cosine similarity of two given sequences in a dictionary format
 
 def cossim(dic1, dic2):
     a = np.array(list(dic1.keys()))
