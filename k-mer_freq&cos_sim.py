bab4f1d8017ccf2d82dd9d691dab2af146ca30ef
Samar Kodwani <samarkodwani9@gmail.com>
2026-05-09 23:16:26 +0530

    Add files via upload

---
+++ k-mer_freq&cos_sim.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def cleaning(text):
+    my_dict = {}
+    with open(text, 'r') as file:
+        for line in file:
+            if line[0] == '>':
+                try:
+                    next_line = next(file)
+                    my_dict[line.strip()] = next_line.strip()
+                    temp = line.strip()
+                except StopIteration:
+                    print("end of the file")
+            else:
+                my_dict[temp] += line.strip()
+    return my_dict
+
+def kf(seq, k):
+    my_dic = {}
+    for i in range(len(seq) - k+1):
+            a = seq[i:i+k]
+            if a not in my_dic:
+                my_dic[a] = 1
+            else:
+                my_dic[a] += 1
+    return my_dic
+
+# dic = cleaning('seqdump.txt')
+# for i, j in dic.items():
+#     print(f'the freq for the sequence ({i}) is:\n')
+#     print(f'{kf(j,3)}\n')
+
+
+
+
+def cossim(dic1, dic2):
+    a = np.array(list(dic1.keys()))
+    b = np.array(list(dic2.keys()))
+    magnitude = np.linalg.norm(np.array(list(dic1.values())))*np.linalg.norm(np.array(list(dic2.values())))
+    c = np.intersect1d(a,b)
+    dot_pro = 0
+    for m in c:
+        dot_pro += dic1[m]*dic2[m]
+    return (dot_pro/magnitude)
+
